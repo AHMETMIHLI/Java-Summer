1fea5c0ef7a1217441a4f8ae5ff36636d2cc51a7
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-08-03 19:50:11 +0300

    Java Summer

---
+++ src/Practice01/Q04_JavaVariable.java
@@ -0,0 +1,30 @@
+package Practice01;
+
+public class Q04_JavaVariable {
+    public static void main(String[] args) {
+        // Initializing Variable  //variable baslangic degeri verme
+        // To copy the variable's value
+        // you can declare multiple variables in the same line
+        // updating a variable
+
+        //Bir degisken tanimlayin : x
+        //Baska bir degiskeni initialize edin : y
+        //x degiskenini initialize edin
+        //y degiskenini yeni bir degerle guncelleyin
+
+        int yil = 2022, ay = 6, gun = 20;
+        System.out.println("yil = " + yil);
+        System.out.println("ay = " + ay);
+        System.out.println("gun = " + gun);
+
+        // updating a variable
+
+        yil = 2032;
+        System.out.println( "guncellenmis hali" +yil);
+        //Bir degisken tanimlayin : x
+        //Baska bir degiskeni initialize edin : y
+        //x degiskenini initialize edin
+        //y degiskenini yeni bir degerle guncelleyin
+
+    }
+}
---
+++ src/Practice01/Q05_EscapeSequences.java
@@ -0,0 +1,4 @@
+package Practice01;
+
+public class Q05_EscapeSequences {
+}
